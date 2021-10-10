e3826525997c1ca11650c18074742a564056c83c
AlenOrbanic <aorbanic@student.unipu.hr>
2021-10-10 23:20:11 +0200

    javascript

---
+++ myscript.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="informatika";
+
+password=prompt('Molimo unesite lozinku',);
+
+if (password==pass1)
+alert('Lozinka točna! kliknite ok kako biste pristupili stranici');
+else
+{
+    window.location="file:///C:/Users/Orba/Desktop/stranica%20prog%20inz/redirect.html";
+}
